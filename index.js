e66bd178c234082e1f5b98bcd2feef0dee14f868
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-09 14:47:32 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const money = 32;
+const income = 6000;
+const addExpenses = "ddddd";
+let deposit = 899999;
+let mission = "learn js";
+let period = "frfrfrfr";
+
+alert('learn');
+console.log('learn js');
