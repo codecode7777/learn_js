b593956ff74581ba44f3dfd5a3bffe1ba13ec207
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-09 19:07:29 +0300

    feat(1): connected lesson2.js to scripts; feat(2): wrote piece of code in lesson2.js

---
+++ scripts/lesson2.js
@@ -0,0 +1,13 @@
+const money = 100000;
+const income = "frilans";
+const addExpenses = "internet, food, lease, utilities";
+const deposit = true;
+const mission = 5000000;
+const period = 4;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцам");
