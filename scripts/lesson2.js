0f58845695ed9dad0058a02f39139a153f411590
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-09 19:38:14 +0300

    feat: completed the task of the second lesson

--- scripts/lesson2.js
+++ scripts/lesson2.js
@@ -1,13 +1,20 @@
-const money = 100000;
+const money = 90000;
 const income = "frilans";
-const addExpenses = "internet, food, lease, utilities";
+const addExpenses = "Internet, Food, Lease, Utilities";
 const deposit = true;
 const mission = 5000000;
 const period = 4;
+const budgetDay = money / 30;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
+
 console.log("Период равен " + period + " месяцам");
+console.log("Цель заработать " + mission + " долларов");
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
