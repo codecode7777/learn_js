0ba2cdc99124b794cdb2dd0cb5b67fde3e875715
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-17 00:29:20 +0300

    feat: final result

--- scripts/lesson3.js
+++ scripts/lesson3.js
@@ -8,22 +8,14 @@ const mission = 5000000;
 const period = 4;
 
 
-
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
 
-// console.log("Период равен " + period + " месяцам");
-// console.log("Цель заработать " + mission + " долларов");
-
-
-
 console.log(addExpenses.toLowerCase().split(', '));
 
-
-//lesson03
 
 const firstCostQuestion = prompt('Введите обязательную статью расходов?');
 const firstCostAnswer = Number(prompt('Во сколько это обойдется?'));
@@ -40,16 +32,6 @@ console.log('Дневной бюджет: ' + Math.floor(budgetDay));
 const time = mission / budgetMonth;
 console.log('Цель будет достигнута за ' + Math.ceil(time) + ' месяцев');
 
-
-/* if (budgetDay > 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay <= 1200 && budgetDay >= 600) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay <= 600 && budgetDay >= 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что то пошло не так');
-} */
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
