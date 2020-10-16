ebff6dee3c9d041ae05883dd938ee15a57b88081
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-16 20:25:45 +0300

    feat: checkpoint

---
+++ scripts/lesson6.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+function mainFunction() {
+    let num = 7;
+
+    function start() {
+        let request = prompt('Угадай число от 1 до 100');
+
+        if (request === null) {
+            return confirm('Игра окончена');
+        } else if (!isNumber(request)) {
+            alert('Введи число!');
+            return start();
+        } else if (request > num) {
+            alert('Загаданное число меньше');
+            return start();
+        } else if (request < num) {
+            alert('Загаданное число больше');
+            return start();
+        } else if (request === num) {
+            return confirm('Поздравляю, Вы угадали!!!');
+        }
+
+
+    }
+
+    start();
+
+}
+
+
+mainFunction();
