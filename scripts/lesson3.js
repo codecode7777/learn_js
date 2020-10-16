9b88323ec60d84894f68499875a4117750020f3a
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-16 18:22:59 +0300

    feat: corrected

--- scripts/lesson3.js
+++ scripts/lesson3.js
@@ -41,12 +41,22 @@ const time = mission / budgetMonth;
 console.log('Цель будет достигнута за ' + Math.ceil(time) + ' месяцев');
 
 
-if (budgetDay > 1200) {
+/* if (budgetDay > 1200) {
     console.log('У вас высокий уровень дохода');
 } else if (budgetDay <= 1200 && budgetDay >= 600) {
     console.log('У вас средний уровень дохода');
 } else if (budgetDay <= 600 && budgetDay >= 0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
 } else {
+    console.log('Что то пошло не так');
+} */
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
     console.log('Что то пошло не так');
 }
