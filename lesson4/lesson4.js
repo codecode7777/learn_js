5ffea3e1754c0f25b650540c378872727d26837c
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-10-17 14:59:39 +0300

    feat: corrected according lesson3

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -15,9 +15,6 @@ let showTypeOf = function(data) {
 };
 
 
-//lesson03
-
-
 const firstCostQuestion = prompt('Введите обязательную статью расходов?');
 const firstCostAnswer = Number(prompt('Во сколько это обойдется?'));
 const secondCostQuestion = prompt('Введите обязательную статью расходов?');
@@ -44,21 +41,18 @@ const budgetDay = Math.ceil(accumulatedMonth / 30);
 
 let getStatusIncome = function() {
 
-    if (budgetDay > 1200) {
+    if (budgetDay >= 1200) {
         return ('У вас высокий уровень дохода');
-    } else if (budgetDay <= 1200 && budgetDay >= 600) {
+    } else if (budgetDay >= 600) {
         return ('У вас средний уровень дохода');
-    } else if (budgetDay <= 600 && budgetDay >= 0) {
+    } else if (budgetDay >= 0) {
         return ('К сожалению у вас уровень дохода ниже среднего');
-    } else {
+    } else if (budgetDay < 0) {
         return ('Что то пошло не так');
     }
 
 };
 
-
-
-//вывод в консоль по порядку как в тз
 
 showTypeOf(deposit);
 
