99c8e4348415682a2d78d9523ed6b6f211eba504
Vyacheslav Zhugin <succesmost1@gmail.com>
2020-11-04 18:12:09 +0300

    feat: -

--- lesson8/lesson8.js
+++ lesson8/lesson8.js
@@ -48,7 +48,8 @@ const appData = {
 
 
         const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        appData.addExpenses = addExpenses.toUpperCase().toString().split(', ');
+        //appData.addExpenses = addExpenses.toUpperCase().toString().split(', ');
+        appData.addExpenses = addExpenses.split(', ');
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
         let question,
             answer;
@@ -149,7 +150,7 @@ console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney
 
 console.log(appData.addExpenses);
 
-for (let key in appData.expenses) {
+for (let key in appData.addExpenses) {
     let myStr = key[0].toLocaleUpperCase() + key.slice(1);
     myStr.toString();
     console.log(myStr);
